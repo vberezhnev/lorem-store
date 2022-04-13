190986fda03a56fa09cf3b6983745a5592bf538e
Tell396 <vova21473@gmail.com>
2022-04-13 23:39:21 +1000

    Story #3. First auth was added.

--- server/index.js
+++ server/index.js
@@ -3,14 +3,20 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
+app.use(fileUpload({}))
 app.use(express.json())
 app.use('/api', router)
+
+app.unsubscribe(errorHandler)
 
 app.get('/', (req, res) => {
     res.status(200).json({ message: 'All is work' })
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,13 @@
 const Router = require('express')
 const router = new Router()
-
-const userRouter = require('./userRouter')
-const typeRouter = require('./typeRouter')
-const brandRouter = require('./brandRouter')
 const deviceRouter = require('./deviceRouter')
-
+const userRouter = require('./userRouter')
+const brandRouter = require('./brandRouter')
+const typeRouter = require('./typeRouter')
 
 router.use('/user', userRouter)
 router.use('/type', typeRouter)
 router.use('/brand', brandRouter)
 router.use('/device', deviceRouter)
-
-
 
 module.exports = router

e194b395f05c759ff64a18b79d4c24bd52b75cf8
Tell396 <vova21473@gmail.com>
2022-04-15 23:55:09 +1000

    Story #5. [FRONTEND] Routing was added.

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import './App.css';
 import AppRouter from "./components/AppRouter";
 
 function App() {
